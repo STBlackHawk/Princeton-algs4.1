938b0acb2dfed7bda8c49a69e5f639ea9ef41348
ShtBlackHawk <taghikhani.sh@gmail.com>
2018-10-16 20:51:08 -0400

    first commit

---
+++ Percolation.java
@@ -0,0 +1,20 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Percolation {
+
+    public Percolation (int n){
+        int [][] matrix = new int[n][n];
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
