b3a257c5a8337c3c6060fb480800c7287bc15257
ShtBlackHawk <taghikhani.sh@gmail.com>
2018-10-18 12:03:15 -0400

    percolates done

--- Percolation.java
+++ Percolation.java
@@ -6,6 +6,7 @@
 
 import java.util.Arrays;
 import java.lang.*;
+import java.util.Collections;
 
 public class Percolation {
     int [][] matrix;
@@ -60,16 +61,68 @@ public class Percolation {
 
     public boolean percolates(){
         int [][] IdMatrix = new int [matrix.length][matrix.length];
-        for (int i= 1; i <= IdMatrix.length; i++){
-            if (matrix[1][i] == 1) {
-                IdMatrix[1][i] = i;
+        int [] IdArray = new int[4];
+
+        for (int i= 1; i < IdMatrix.length; i++){
+            for ( int j=0; j < IdMatrix.length; j++){
+
+                if (matrix[i][j] == 0) {
+                    IdMatrix[i][j] = i*matrix.length + j;
+                    IdArray[0] = IdMatrix[i][j] ;
+                }
+                if(matrix[i-1][j] == 0){
+                    IdArray[1] = IdMatrix[i-1][j];
+                }
+                if(matrix[i][j-1] == 0){
+                    IdArray[2] = IdMatrix[i][j+1];
+                }
+                if(matrix[i][j+1] == 0){
+                    IdArray[3] = IdMatrix[i][j+1];
+                }
+                int min = IdArray[0];
+                for ( int k = 0; k<4; k++){
+                    if (IdArray[k] < min){
+                        min = IdArray[k];
+                    }
+                }
+                IdMatrix[i][j] = min;
             }
+            for ( int j=matrix.length; j > 0; j--){
+
+                if (matrix[i][j] == 0) {
+                    IdArray[0] = IdMatrix[i][j] ;
+                }
+                if(matrix[i-1][j] == 0){
+                    IdArray[1] = IdMatrix[i-1][j];
+                }
+                if(matrix[i][j-1] == 0){
+                    IdArray[2] = IdMatrix[i][j+1];
+                }
+                if(matrix[i][j+1] == 0){
+                    IdArray[3] = IdMatrix[i][j+1];
+                }
+                int min = IdArray[0];
+                for ( int k = 0; k<4; k++){
+                    if (IdArray[k] < min){
+                        min = IdArray[k];
+                    }
+                }
+                IdMatrix[i][j] = min;
+            }
+
+
+
+            
+
         }
+
+
+
 
         for (int j = 2; j <= IdMatrix.length ;  j++){
             for (int k = 1 ; k<= IdMatrix.length; k++){
                 if (matrix[j-1][k] == 1 ){
-                    IdMatrix[j][k] = IdMatrix[j-1][k];
+
 
                 }
             }
