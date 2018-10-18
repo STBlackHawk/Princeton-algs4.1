3893836d7b09b64d256508b1363b8606d5735c76
ShtBlackHawk <taghikhani.sh@gmail.com>
2018-10-18 12:51:34 -0400

    Writing percolates using IsFull

--- Percolation.java
+++ Percolation.java
@@ -7,9 +7,13 @@
 import java.util.Arrays;
 import java.lang.*;
 import java.util.Collections;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     int [][] matrix;
+    int [][] IdMatrix ;
 
     public Percolation (int n){
         if ( n < 1) throw java.lang.IllegalArgumentException("n should be >= 1");
@@ -39,29 +43,9 @@ public class Percolation {
     }
 
     public boolean isFull (int row, int col){
-        if (matrix[row][col] > 0 ) {
-            return true;
-        }
-
-        return false;
-
-    }
-
-    public int numberOfOpenSites(){
-        int m = 0;
-        for (int j = 0; j < matrix.length ;  j++){
-            for (int k = 0 ; k< matrix.length; k++){
-                if (matrix[j][k] == 0 ){
-                    m++;
-                }
-            }
-        }
-        return m;
-    }
-
-    public boolean percolates(){
-        int [][] IdMatrix = new int [matrix.length][matrix.length];
-        int [] IdArray = new int[4];
+        IdMatrix = new int [matrix.length][matrix.length];
+        int [] IdArray = new int[5];
+        boolean b = false;
 
         for (int i= 1; i < IdMatrix.length; i++){
             for ( int j=0; j < IdMatrix.length; j++){
@@ -79,59 +63,93 @@ public class Percolation {
                 if(matrix[i][j+1] == 0){
                     IdArray[3] = IdMatrix[i][j+1];
                 }
+                if(matrix[i+1][j] == 0){
+                    IdArray[4] = IdMatrix[i+1][j];
+                }
+
                 int min = IdArray[0];
-                for ( int k = 0; k<4; k++){
+                for ( int k = 0; k<5; k++){
                     if (IdArray[k] < min){
                         min = IdArray[k];
                     }
                 }
                 IdMatrix[i][j] = min;
             }
-            for ( int j=matrix.length; j > 0; j--){
+            for ( int j=matrix.length; j > 0; j--) {
 
                 if (matrix[i][j] == 0) {
-                    IdArray[0] = IdMatrix[i][j] ;
+                    IdArray[0] = IdMatrix[i][j];
                 }
-                if(matrix[i-1][j] == 0){
-                    IdArray[1] = IdMatrix[i-1][j];
+                if (matrix[i - 1][j] == 0) {
+                    IdArray[1] = IdMatrix[i - 1][j];
                 }
-                if(matrix[i][j-1] == 0){
-                    IdArray[2] = IdMatrix[i][j+1];
+                if (matrix[i][j - 1] == 0) {
+                    IdArray[2] = IdMatrix[i][j + 1];
                 }
-                if(matrix[i][j+1] == 0){
-                    IdArray[3] = IdMatrix[i][j+1];
+                if (matrix[i][j + 1] == 0) {
+                    IdArray[3] = IdMatrix[i][j + 1];
                 }
+
+                if (matrix[i + 1][j] == 0) {
+                    IdArray[4] = IdMatrix[i + 1][j];
+                }
+
                 int min = IdArray[0];
-                for ( int k = 0; k<4; k++){
-                    if (IdArray[k] < min){
+                for (int k = 0; k < 4; k++) {
+                    if (IdArray[k] < min) {
                         min = IdArray[k];
                     }
                 }
                 IdMatrix[i][j] = min;
             }
 
-
-
-            
-
         }
 
+        if (matrix[row][col] == 0 ) {
+            for(int m = 0; m < matrix.length;m ++ )
+                if (IdMatrix[row][col]==IdMatrix[0][m])
+            b = true;
+        }
+        else {
 
+             b = false;
+        }
+        return b;
+    }
 
-
-        for (int j = 2; j <= IdMatrix.length ;  j++){
-            for (int k = 1 ; k<= IdMatrix.length; k++){
-                if (matrix[j-1][k] == 1 ){
-
-
+    public int numberOfOpenSites(){
+        int m = 0;
+        for (int j = 0; j < matrix.length ;  j++){
+            for (int k = 0 ; k< matrix.length; k++){
+                if (matrix[j][k] == 0 ){
+                    m++;
                 }
             }
         }
+        return m;
     }
+
+    public boolean percolates() {
+       boolean b = false;
+        for (int m = 1; m < IdMatrix.length; m++) {
+            b = isFull(matrix.length -1, m);
+
+        }
+        return b;
+    }
+
 
     public static void main(String[] args) {
 
 
+    }
+
+
+    public class PercolationStats{
+        public PercolationStats(int n, int trials){
+
+
+        }
 
     }
 }
